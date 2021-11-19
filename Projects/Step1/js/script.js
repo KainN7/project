13ce80b0b3e39a48cb82e8c90da522910c31f1bf
Alexander <sasha3310@mail.ru>
2021-11-19 23:52:46 +0700

    add obj

--- Projects/Step1/js/script.js
+++ Projects/Step1/js/script.js
@@ -1,3 +1,11 @@
 'use strict';
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
-console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count:
+    movies:
+    actors:
+    genres:
+    privat:
+}
+
