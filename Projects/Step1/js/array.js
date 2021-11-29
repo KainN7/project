e34f41b295beff1a60b74b0124168eca7693df47
Alexander <sasha3310@mail.ru>
2021-11-30 00:16:23 +0700

    add array.js

---
+++ Projects/Step1/js/array.js
@@ -0,0 +1,36 @@
+// 'use strict';
+
+// const arr = [0, 41, 2, 53, 6, 8];     // массив служит для того, чтобы данные хранились строго по порядку.
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {          // у псевдамассивов нет методов.
+//     return a - b;
+// }
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// arr.pop(); // удаляет элемент с конца массива.
+// arr.push(10); // добавляет элемент в конец массива.
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);        // можно использовать брейк и континью.
+// }                            
+
+// const str = prompt('', '');       
+// const products = str.split(', ');   // можем сформировать массив на основании строк.
+// products.sort(); // сортирует в алфавитном порядке.  Но всегда как строки
+// console.log(products.join('; '));  // на основании массива можем сформировать большую строку.
+
