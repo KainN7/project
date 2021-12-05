2837506564edb2243ec61f52dd0cf8d83b09a435
Alexander <sasha3310@mail.ru>
2021-12-06 00:29:19 +0700

    Carried out actions with elements on the page, worked with the DOM

--- Elements_25/elements/js/script.js
+++ Elements_25/elements/js/script.js
@@ -1,23 +1,68 @@
 'use strict';
 
-const box = document.getElementById('box');
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'), // из псевдо массива получили одну кнопку.
+      circles = document.getElementsByClassName('circle'),
+      wrapper = document.querySelector('.wrapper'),
+      hearts = wrapper.querySelectorAll('.heart'), // в эти переменные пойдут только те элементы, которые подойдут по 
+      oneHeart = wrapper.querySelector('.heart');  // определенному селектору и находятся только внутри этого 
+      // элемента.
 
-console.log(box);
+// box.style.backgroundColor = 'blue'; //меняем inline style. они всегда перибивают все остальные.
+// box.style.width = '500px';
 
-const btns = document.getElementsByTagName('button'); // из псевдо массива получили одну кнопку.
+box.style.cssText = 'background-color: blue; width: 500px';
 
-console.log(btns[0]);
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red'; // укаазываем чётко элемент, псевдомассив не знает про style,
+// просто структура, которая содержит элементы.
 
-const circles = document.getElementsByClassName('circle');
-
-console.log(circles);
-
-const hearts = document.querySelectorAll('.heart');
+// for (let i = 0; i < hearts.length; i++) {        // используем редко.
+//     hearts[i].style.backgroundColor = 'blue';
+// }
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = 'blue'; // item так назвали сердечки.
 });
 
-const oneHeart = document.querySelector('div');
-console.log(oneHeart);
+const div = document.createElement('div'); // существует только внутри JS.
+// const text = document.createTextNode('Тут был я');
+
+div.classList.add('black'); // добавляем из css.
+
+wrapper.append(div);
+
+// Работаем с DOM.
+// document.body.append(div);  // положили в конец body наш div.
+// wrapper.appendChild(div); Устаревшая конструкция.
+
+// document.querySelector('.wrapper').append(div); // положили в конец wrapper.
+
+// wrapper.append(div); // тоже самое что и выше при условии что использовали DOM.
+
+// wrapper.prepend(div); // вставляет в начало.
+
+// hearts[0].before(div); // перед 1 сердечком вставился черный квадратик.
+// hearts[0].after(div); // после 1 сердечка вставился черный квадратик.
+
+// wrapper.insertBefore(div, hearts[0]); // Устаревший метод.
+
+// circles[0].remove(); // удалили 1 кружочек.
+// wrapper.removeChild(hearts[1]); // Устаревший метод.
+
+hearts[0].replaceWith(circles[0]); // заменили сердце кружочком.
+// wrapper.replaceChild(circles[0], hearts[0]); // Устаревший метод.
+
+// div.innerHTML = '<h1>Hello World</h1>'; // в прямоугольник запихали h1 и текст.
+// работаем с HTML структурой, пишем innerHTML.
+
+// div.textContent = '<h1>Hello World</h1>'; // работает только с текстом. HTML структура не войдет, теги пихать нельзя.
+// Хотим записать только текст, используем textContent
+
+div.innerHTML = '<h1>Hello World</h1>';
+// div.insertAdjacentHTML('beforebegin', '<h2>Hello</h2>'); // вставили код перед элементом.
+// div.insertAdjacentHTML('afterbegin', '<h2>Hello</h2>'); // вставили код в начало элемента
+// div.insertAdjacentHTML('beforeend', '<h2>Hello</h2>'); // вставили код в конец элемента.
+// div.insertAdjacentHTML('afterend', '<h2>Hello</h2>'); // вставляет код после элемента.
+
 
