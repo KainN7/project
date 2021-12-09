27f411d5f18a626873cdfb25a41c4f9255e18697
Alexander <sasha3310@mail.ru>
2021-12-09 21:14:49 +0700

    changed script.js in projectFilms

--- projectFilms/js/script.js
+++ projectFilms/js/script.js
@@ -24,3 +24,37 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'), // искал рекламные блоки.
+      poster = document.querySelector('.promo__bg'),  // такой элемент всего один.
+      genre = poster.querySelector('.promo__genre'), // Нашли жанр внутри  постера.
+      movieList = document.querySelector('.promo__interactive-list');                                   
+                                            //Нашел список фильмов.
+adv.forEach(item =>{   // стрелочная функция. Удалили рекламные блоки со страницы.
+    item.remove();     // она предпочтительнее.
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';     // использовать разные виды кавычек особенно касается 
+                                                        // css свойств.
+movieList.innerHTML = '';  // очистили HTML структуру.
+
+movieDB.movies.sort(); // отсортировали по алфавиту.
+
+// console.log(poster.innerHTML); // позволяет так же получать элементы со страницы.
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `  
+        <li class="promo__interactive-item">${i + 1} ${film}
+                <div class="delete"></div>
+        </li>
+    `;
+}); // динамически сформировали список и поместили егона страницу, с нумерацией по
+    // порядку.
+
+
+// adv.forEach(function(item) {   // функция обычная сделали то же что и выше.
+//     item.remove();
+// });
+
+
