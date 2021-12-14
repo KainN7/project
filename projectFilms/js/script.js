a1bbb6c9768d48d95e8982994594242cc1556f1c
Alexander <sasha3310@mail.ru>
2021-12-14 14:30:40 +0700

    refactor script js.

--- projectFilms/js/script.js
+++ projectFilms/js/script.js
@@ -14,47 +14,51 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-const adv = document.querySelectorAll('.promo__adv img'), // искал рекламные блоки.
-      poster = document.querySelector('.promo__bg'),  // такой элемент всего один.
-      genre = poster.querySelector('.promo__genre'), // Нашли жанр внутри  постера.
-      movieList = document.querySelector('.promo__interactive-list');                                   
-                                            //Нашел список фильмов.
-adv.forEach(item =>{   // стрелочная функция. Удалили рекламные блоки со страницы.
-    item.remove();     // она предпочтительнее.
+document.addEventListener('DOMContentLoaded', () => {  // дожидаемся построение DOM структуры, 
+// чтобы скрипты нормально запустились и не было ошибки. Код ниже сработаает после загрузки DOM структуры.
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelectorAll('.promo__adv img'), // искал рекламные блоки.
+          poster = document.querySelector('.promo__bg'),  // такой элемент всего один.
+          genre = poster.querySelector('.promo__genre'), // Нашли жанр внутри  постера.
+          movieList = document.querySelector('.promo__interactive-list');                                   
+                                                //Нашел список фильмов.
+    adv.forEach(item =>{   // стрелочная функция. Удалили рекламные блоки со страницы.
+        item.remove();     // она предпочтительнее.
+    });
+    
+    genre.textContent = 'драма';
+    
+    poster.style.backgroundImage = 'url("img/bg.jpg")';     // использовать разные виды кавычек особенно касается 
+                                                            // css свойств.
+    movieList.innerHTML = '';  // очистили HTML структуру.
+    
+    movieDB.movies.sort(); // отсортировали по алфавиту.
+    
+    // console.log(poster.innerHTML); // позволяет так же получать элементы со страницы.
+    
+    movieDB.movies.forEach((film, i) => {
+        movieList.innerHTML += `  
+            <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+            </li>
+        `;  // динамически сформировали список и поместили егона страницу, с нумерацией по
+        // порядку.
+    }); 
 });
-
-genre.textContent = 'драма';
-
-poster.style.backgroundImage = 'url("img/bg.jpg")';     // использовать разные виды кавычек особенно касается 
-                                                        // css свойств.
-movieList.innerHTML = '';  // очистили HTML структуру.
-
-movieDB.movies.sort(); // отсортировали по алфавиту.
-
-// console.log(poster.innerHTML); // позволяет так же получать элементы со страницы.
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `  
-        <li class="promo__interactive-item">${i + 1} ${film}
-                <div class="delete"></div>
-        </li>
-    `;
-}); // динамически сформировали список и поместили егона страницу, с нумерацией по
-    // порядку.
-
 
 // adv.forEach(function(item) {   // функция обычная сделали то же что и выше.
 //     item.remove();
 // });
+
+
 
 
