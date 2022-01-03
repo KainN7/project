9b0077a8513b18d236582377553dae0fd6bd5196
Alexander <sasha3310@mail.ru>
2022-01-03 13:25:58 +0700

    refactor script.js

--- preview/script.js
+++ preview/script.js
@@ -15,6 +15,19 @@ console.log(width, height);
 
 btn.addEventListener('click', () => {
     // после клика устанавливаему бокса полную высоту с учетом всей прокрутки.
-    // box.style.height = box.scrollHeight + 'px'; 
-    console.log(box.scrollTop);
+    box.style.height = box.scrollHeight + 'px'; 
+    // console.log(box.scrollTop);
 });
+
+// В JS координаты начинаются в левом верхнем углу экрана.
+
+console.log(box.getBoundingClientRect().top); //  возвращает размер элемента и его позицию
+
+const style = window.getComputedStyle(box, ); // возвращает объект, содержащий значения всех CSS-свойств
+
+console.log(style.display); // получаем значение в данном случае display block
+
+// При помощи JS мы не можем работать с псевдоэлементами, мы их ни как не получим со страницы
+// Но стили псевдоэлементов мы поучить можем.
+
+console.log(document.documentElement.scrollTop);
