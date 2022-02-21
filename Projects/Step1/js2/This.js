45157f50122371fa041a93ade0010859cad2552f
Alexander <sasha3310@mail.ru>
2022-02-21 18:27:43 +0700

    refactor This.js and main.css

--- Projects/Step1/js2/This.js
+++ Projects/Step1/js2/This.js
@@ -25,17 +25,49 @@
 // };
 // obj.sum();
 
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log("Hello! " + this.name);
-    };
-}
-let ivan = new User('Ivan', 23);
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log("Hello! " + this.name);
+//     };
+// }
+// let ivan = new User('Ivan', 23);
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: 'John'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
+// bind создаёт новую функцию и под нее подвязывает контекст.
+
 
 
 // 1) Обычная функция: this = window, но если стоит 'use strict',
 // то будет undefined.
 // 2) Контекст у методов объекта - сам объект
+// 3) this в конструкторах и классах - это новый экземпляр объекта
+// 4) Ручная привязка this: call, apply, bind.
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    this.style.backgroundColor = 'red';
+});
+// Когда обработчик событий написан в классическом режиме function()
+// тогда контекст вызова будет сам элемент на котором произошло событие.
