1cdb8c801050786f873db10503f27e22ecbe1bd8
Alexander <sasha3310@mail.ru>
2022-02-22 17:39:34 +0700

    refafctor Class(ES6).js

--- Projects/Step1/js2/Class(ES6).js
+++ Projects/Step1/js2/Class(ES6).js
@@ -1,2 +1,18 @@
 'use strict';
 
+// класс всегда пишется с заглавной буквы
+
+ class Rectangle {
+    constructor(height, width) {
+        this.height = height;   // свойства класса
+        this.width = width;
+    }
+
+    calcArea() {  // пример метода 'возвращает площать прямоугольника'
+        return this.height * this.width;
+    }
+ }
+
+ const square = new Rectangle(10, 10);
+
+ console.log(square.calcArea());
